c3dde57d670f076d54e560f4e379530e2b36a3df
mdrazuahamad <mdrazuahamad8@gmail.com>
2025-05-06 21:17:22 +0600

    mainjsx file

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,13 +1,12 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css"; // or your global styles
+import "./index.css"; // or your main CSS file
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
